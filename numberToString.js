0ea85e1d299dd2836da1e68422fb01933246117c
Dalip Yadav <dalipyadav68@gmail.com>
2024-02-06 21:07:10 +0530

    numbertostring

---
+++ numberToString.js
@@ -0,0 +1,14 @@
+//Number to String
+
+
+//Converting from number to string
+///Though rarely used, you can convert a number to a string by calling the .toString() method.
+
+
+function convertNumberToString(number) {
+    return number.toString();
+
+}
+console.log(convertNumberToString(42)); // "42"
+console.log(convertNumberToString(97)); // "97"
+console.log(convertNumberToString(11)); // "11"
